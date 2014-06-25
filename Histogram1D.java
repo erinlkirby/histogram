a4764f76bb2316fa84f83165b70309603f9c2461
Erin Kirby <erin.kirby.11@cnu.edu>
2014-06-25 15:00:52 -0400

    Added a brute force method for adaptive binning, made it possible to increment bin content without knowing which bin a value is in, and made it possible to initialize a Histogram1D object without specifying a number of bins, making a bin for each 1.0 width value in range.

--- Histogram1D.java
+++ Histogram1D.java
@@ -1,10 +1,13 @@
 package org.jlab.cnuphys.histogram;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  * Defines the class to create a basic 1D Histogram
  * 
  * @author Erin Kirby
- * @version 061714
+ * @version 062514
  */
 public class Histogram1D {
 
@@ -26,6 +29,7 @@ public class Histogram1D {
     	setName("default");
     	setTitle("default");
     	set(1,0.0,1.0);
+    	initDataStore(1);
     }
     
     /**
@@ -44,6 +48,7 @@ public class Histogram1D {
     	setYTitle(yTitle);
     	setName(hName);
     	set(bins, xMin, xMax);
+    	initDataStore(bins);
     }
     
     /**
@@ -58,7 +63,6 @@ public class Histogram1D {
     public Histogram1D(String name, double xMin, double xMax, double[] binHeights) {
     	setName(name);
     	set(binHeights.length, xMin, xMax);
-    	histogramData = new double[binHeights.length];
     	for (int i = 0; i < binHeights.length; i++) {
     		histogramData[i] = binHeights[i];
     	}
@@ -92,6 +96,20 @@ public class Histogram1D {
     	setName(name);
     	setTitle(title);
     	set(bins, xMin, xMax);
+    }
+    
+    /**
+     * Creates a new histogram using the name, title, minimum, and maximum.
+     * 
+     * @param name		The desired name of the histogram
+     * @param title		The desired title of the histogram
+     * @param xMin		The desired minimum x-axis value
+     * @param xMax		The desired maximum x-axis value
+     */
+    public Histogram1D(String name, String title, double xMin, double xMax) {
+    	setName(name);
+    	setTitle(title);
+    	set((int)(xMax - xMin), xMin, xMax);
     }
     
     /**
@@ -272,30 +290,28 @@ public class Histogram1D {
     }
     
     /**
-     * Increments the content in the specified bin by one. The bin is specified in array indexing
-     * format (to increment the 1st bin, enter 0, the 2nd, enter 1, ... , the nth, enter n-1)
+     * Finds the bin that holds that value in its margins 
+     * and increments it by one.
      * 
-     * @param bin		the bin to be incremented, specified in array indexing format.
+     * @param val		the value to find which bin holds it.
      */
-    public void incrementBinContent(int bin) {
-    	if (bin >= 0 && bin < histogramData.length) {
-    		histogramData[bin] = histogramData[bin] + 1;
-    		histogramDataError[bin] = Math.sqrt(Math.abs(histogramData[bin]));
-    	}
+    public void incrementBinContent(double val) {
+    	int bin = xAxis.getBin(val);
+    	histogramData[bin] += 1;
+    	histogramDataError[bin] = Math.sqrt(histogramData[bin]);
     }
     
     /**
-     * Increments the content in the specified bin by the entered weight. The bin is specified in array indexing
-     * format (to increment the 1st bin, enter 0, the 2nd, enter 1, ... , the nth, enter n-1)
+     * Finds the bin with that value between its margins
+     * and adds the specified weight
      * 
-     * @param bin		the bin to be incremented, specified in array indexing format.
-     * @param weight	the weight to increment by
+     * @param val		The value to add the weight to
+     * @param weight	The weight to add to that value
      */
-    public void incrementBinContent(int bin, double weight) {
-    	if (bin >= 0 && bin < histogramData.length) {
-    		histogramData[bin] = histogramData[bin] + weight;
-    		histogramDataError[bin] = Math.sqrt(Math.abs(histogramData[bin]));
-    	}
+    public void incrementBinContent(double val, double weight) {
+    	int bin = xAxis.getBin(val);
+    	histogramData[bin] += weight;
+    	histogramDataError[bin] = Math.sqrt(histogramData[bin]);
     }
     
     /**
@@ -334,6 +350,7 @@ public class Histogram1D {
     public void setBinContent(int bin, double value) {
     	if ((bin >= 0) && (bin < histogramData.length)) {
     		histogramData[bin] = value;
+    		histogramDataError[bin] = Math.sqrt(Math.abs(histogramData[bin]));
     	}
     }
     
@@ -346,7 +363,7 @@ public class Histogram1D {
      * @param value		the error to store in the specified bin
      */
     public void setBinError(int bin, double value) {
-    	if (bin <= 0 && bin < histogramDataError.length) {
+    	if (bin >= 0 && bin < histogramDataError.length) {
     		histogramDataError[bin] = value;
     	}
     }
@@ -441,13 +458,80 @@ public class Histogram1D {
         return graph;
     }
     
+    /**
+     * 
+     * @return		the data in the histogram
+     */
     public double[] getData() {
     	return histogramData;
     }
     
+    /**
+     * 
+     * @return		the data error in the histogram
+     */
     public double[] getDataError() {
     	return histogramDataError;
     }
     
-    
+    /**
+     * Changes the bin widths to vary with a set minimum slope to 
+     * be allowed as its own bin.
+     * 
+     * @param	sensitivity			what percentage of the maximum slope
+     * 								all bins have to be at minimum to be 
+     * 								considered its own distinct bin
+     */
+    public void fixBinWidths(double sensitivity) {
+    	double maxSlope = Math.abs(histogramData[1] - histogramData[0]);
+    	for (int i = 1; i < histogramData.length - 1; i++) {
+    		double slope = Math.abs(histogramData[i+1] - histogramData[i]);
+    		if (slope > maxSlope) {
+    			maxSlope = slope;
+    		}
+    	}
+    	
+    	double minSlope = maxSlope * sensitivity; //allows bin slope to
+    									  		  //be as little as that 
+    											  //magnitude of the max
+    	double[] histData = new double[histogramData.length];
+    	double[] histMargins = new double[xAxis.axisMargins.length];
+    	
+    	for (int i = 0; i < histData.length; i++) {
+    		histData[i] = -1.0;
+    	}
+    	
+    	histData[0] = histogramData[0];
+    	histMargins[0] = xAxis.axisMargins[0];
+    	int index = 0;
+    	for (int i = 0; i < histogramData.length - 1; i++) {
+    		if (Math.abs(histogramData[i+1] - histogramData[i]) < minSlope) {
+    			if (histData[index] == -1.0) {
+    				histData[index] = 0.0;
+    			}
+    			histData[index] += histogramData[i+1];
+    			histMargins[index+1] = xAxis.axisMargins[i+2];
+    		}
+    		
+    		else {
+    			histData[index+1] = histogramData[i+1];
+    			histMargins[index+1] = xAxis.axisMargins[i+1];
+    			index++;
+    		}
+    	}
+    	
+    	set(index + 1, xAxis.min(), xAxis.max());
+    	
+    	for (int i = 0; i < histogramData.length; i++) {
+    		setBinContent(i, histData[i]);
+    		xAxis.set(histMargins);
+    	}
+    	
+    	for (int i = 0; i < histogramData.length - 1; i++) {
+    		if (Math.abs(histogramData[i+1] - histogramData[i]) < minSlope) {
+    			fixBinWidths(sensitivity);
+    			break;
+    		}
+    	}
+    }
 }
